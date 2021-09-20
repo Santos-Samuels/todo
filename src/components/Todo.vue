574392ed93bb34dc4e32e544e73dea21cced97cf
Santos-Samuels <saamsmith15@gmail.com>
2021-09-19 23:18:51 -0300

    master

---
+++ src/components/Todo.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="p-2 px-3 border shadow-sm mt-3" :class="{checked: todo.checked}">
+        <article class="d-flex align-items-center justify-content-between">
+          <div class="me-3 text-start d-flex align-items-center">
+            <i class="bi me-2 fs-5" :class="todo.checked ? 'bi-check-lg' : 'bi-clock'"></i>
+            <span>{{ todo.description }}</span>
+          </div>
+
+          <div class="text-decoration-none text-nowrap">
+            <span class="btn btn-outline-primary me-2" @click="$emit('toggle', todo)">
+                <span v-if="todo.checked">Concluído</span>
+                <span v-else>Desmarcar</span>
+            </span>
+            <span class="btn btn-outline-danger" @click="$emit('remove', todo)">Remover</span>
+          </div>
+        </article>
+      </div>
+</template>
+
+<script>
+export default {
+    props: { 
+        todo: { type: Object, required: true }
+    }
+}
+</script>
+
+<style scoped>
+    .checked {
+        text-decoration: line-through;
+        color: lightgray;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <h1 class="fw-bolder">TODO</h1>
+    </div>
+
+    <form @submit.prevent="addTodo(todo)">
+      <div class="input-group mt-4">
+        <input class="form-control" v-model="todo.description" type="text" name="todoContent" id="todoContent">
+        <button class="btn btn-primary">Adicionar</button>
+      </div>
+    </form>
+
+    <section class="mt-5">
+      <Todo v-for="t in todos" :key="t.id" @toggle="toggleTodo" @remove="removeTodo" :todo="t"/>
+    </section>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Todo from '@/components/Todo.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  components: { Todo },
+  data() {
+    return { todos: [], todo: {checked: false} }
+  },
+  methods: {
+    addTodo(todo) {
+      todo.id = Date.now()
+      this.todos.push(todo)
+      this.todo = { checked: false }
+    },
+
+    toggleTodo(todo) {
+      const index = this.todos.findIndex(item => item.id === todo.id)
+
+      if(index > -1) {
+        const checked = !this.todos[index].checked
+        this.todos[index] = { ...this.todos[index], checked }
+      }
+    },
+
+    removeTodo(todo) {
+      const index = this.todos.findIndex(item => item.id === todo.id)
+
+      if(index > -1) {
+        // this.todos.splice(index, 1)
+        delete(this.todos, index)
+      }
+    }
   }
 }
 </script>
+
+<style scoped>
+  .container {
+    max-width: 600px !important;
+  }
+</style>
